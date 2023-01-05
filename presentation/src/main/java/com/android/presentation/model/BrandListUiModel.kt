5099a5cc4c3b00b6fd3066dbc438237f1ed76777
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-05 17:59:53 +0900

    Issues #5 refactor: reviewKtlint 제거
    - 현재 설정이 120이라서 reviewKtlint 적합하지 않아서 제거

---
+++ presentation/src/main/java/com/android/presentation/model/BrandListUiModel.kt
@@ -0,0 +1,5 @@
+package com.android.presentation.model
+
+data class BrandListUiModel(
+    private val test: String = "가나다"
+)
--- presentation/src/main/java/com/android/presentation/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/android/presentation/ui/main/MainViewModel.kt
@@ -1,8 +1,26 @@
 package com.android.presentation.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.domain.usecase.FetchBroadCategoryUseCase
+import com.android.domain.usecase.FetchBroadListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : ViewModel()
+class MainViewModel @Inject constructor(
+    private val fetchBroadListUseCase: FetchBroadListUseCase,
+    private val fetchBroadCategoryUseCase: FetchBroadCategoryUseCase
+) : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+            val broadList = fetchBroadListUseCase()
+            val category = fetchBroadCategoryUseCase()
+            Log.d("TAG", "방송 리스트 -> $broadList ")
+            Log.d("TAG", "카테고리 리스트 -> $category ")
+        }
+    }
+}
