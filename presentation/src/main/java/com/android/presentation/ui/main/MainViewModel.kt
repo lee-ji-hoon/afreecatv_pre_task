acf2cf8a526ba5a62c9c8392fcf5c1f010c2b9cf
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-05 10:21:06 +0900

    Issues #5 fix: HiltViewModel을 사용하기 위해 Inject 추가

--- presentation/src/main/java/com/android/presentation/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/android/presentation/ui/main/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.android.presentation.ui.main
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel : ViewModel()
+class MainViewModel @Inject constructor() : ViewModel()
