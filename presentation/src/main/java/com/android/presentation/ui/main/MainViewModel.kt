9e6c6c12df47fff352b0549cff5cf2a8c01759ff
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-05 10:12:25 +0900

    Issues #5 feat: 패키지 변경 및 Main Presentation 계층 구조
    - Hilt 적용
    - 패키지 변경으로 인한 menifest 정보 값 변경

---
+++ presentation/src/main/java/com/android/presentation/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.android.presentation.ui.main
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MainViewModel : ViewModel()
