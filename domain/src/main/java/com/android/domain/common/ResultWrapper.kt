b55738c8c1df3dbb82f46b3d6389d7c4eb89c332
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-05 17:03:00 +0900

    Issues #5 feat: SafeApiCall 모듈 구현
    - API 요청 중복 코드를 최소화 하기 위함
    - Mapper 하는 과정 최소화하기 위해서 공용 ConvertMapper 구현

---
+++ domain/src/main/java/com/android/domain/common/ResultWrapper.kt
@@ -0,0 +1,19 @@
+package com.android.domain.common
+
+sealed class ResultWrapper<T>(
+    val data: T? = null,
+    val error: ErrorData? = null,
+    val code: Int? = null
+) {
+    class Success<T>(data: T, code: Int? = null) : ResultWrapper<T>(data = data, code = code)
+    class Failed<T>(error: ErrorData, data: T? = null) : ResultWrapper<T>(data = data, error = error)
+    class FetchLoading<T>(data: T? = null) : ResultWrapper<T>(data = data)
+
+    override fun toString(): String {
+        return when (this) {
+            is Success -> "Success [data=$data]"
+            is Failed -> "Error[exception=${error?.message}]"
+            is FetchLoading -> "Fetching Loading"
+        }
+    }
+}
---
+++ safecall/src/main/java/com/android/safecall/common/SafeApi.kt
@@ -0,0 +1,38 @@
+package com.android.safecall.common
+
+import com.android.domain.common.ErrorData
+import com.android.domain.common.ResultWrapper
+import com.android.safecall.error.ErrorHandlerImpl
+import retrofit2.Response
+
+abstract class SafeApi : ErrorHandlerImpl() {
+
+    override suspend fun <ResultType, RequestType> getSafe(
+        remoteFetch: suspend () -> Response<RequestType>,
+        mapping: (RequestType) -> ResultType
+    ): ResultWrapper<ResultType> = handleResponse({
+        remoteFetch()
+    }, mapping)
+
+    private suspend fun <RequestType, ResultType> handleResponse(
+        call: suspend () -> Response<RequestType>,
+        converter: (RequestType) -> ResultType
+    ): ResultWrapper<ResultType> {
+        return try {
+            val response = call()
+            if (response.isSuccessful) {
+                response.body()?.let {
+                    return ResultWrapper.Success(
+                        data = converter(it),
+                        code = response.code()
+                    )
+                }
+            }
+            return ResultWrapper.Failed(
+                error = ErrorData.Api(response.errorBody()?.string())
+            )
+        } catch (t: Throwable) {
+            ResultWrapper.Failed(getError(t))
+        }
+    }
+}
