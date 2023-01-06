bdee60e95d94b17889cb3c1c1637768a157c6833
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-06 17:05:12 +0900

    Issues #7 feat: fetchBroadList 반환 값 수정 및  카테고리 값 추가

--- data/src/main/java/com/android/data/datasource/BrodRemoteDataSourceImpl.kt
+++ data/src/main/java/com/android/data/datasource/BrodRemoteDataSourceImpl.kt
@@ -2,11 +2,11 @@ package com.android.data.datasource
 
 import com.android.data.NetworkApiService
 import com.android.data.mapper.ConvertMapper
-import com.android.data.model.BroadCategoryListData
-import com.android.data.model.BroadListData
+import com.android.data.model.BroadCategoryData
+import com.android.data.model.BroadData
 import com.android.domain.common.ResultWrapper
+import com.android.domain.model.Broad
 import com.android.domain.model.BroadCategory
-import com.android.domain.model.BroadList
 import com.android.safecall.common.SafeApi
 import javax.inject.Inject
 
@@ -14,16 +14,22 @@ class BrodRemoteDataSourceImpl @Inject constructor(
     private val apiService: NetworkApiService
 ) : BroadRemoteDataSource, SafeApi() {
 
-    override suspend fun fetchBroadList(): ResultWrapper<BroadList> = getSafe(
-        remoteFetch = { apiService.fetchBroadList() },
-        mapping = { response -> ConvertMapper<BroadListData, BroadList>()(response) }
+    override suspend fun fetchBroadList(categoryName: String): ResultWrapper<List<Broad>> = getSafe(
+        remoteFetch = { apiService.fetchBroadList(categoryName = categoryName) },
+        mapping = { response ->
+            response.broad.map {
+                ConvertMapper<BroadData, Broad>()(
+                    it
+                )
+            }
+        }
     )
 
     override suspend fun fetchBrandCategoryList(): ResultWrapper<List<BroadCategory>> = getSafe(
         remoteFetch = { apiService.fetchBroadCategoryList() },
         mapping = { response ->
             response.categoryList.map {
-                ConvertMapper<BroadCategoryListData.BroadCategoryData, BroadCategory>()(
+                ConvertMapper<BroadCategoryData, BroadCategory>()(
                     it
                 )
             }
