9305ddf04d7f050c419698ec78dc66e66dade90c
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-07 14:52:04 +0900

    Issues #9 feat: 썸네일 이미지 gif 적용, 인원수 DecimalFormat 적용

--- presentation/src/main/java/com/android/presentation/ui/common/bindingadapter/ImageBindingAdapter.kt
+++ presentation/src/main/java/com/android/presentation/ui/common/bindingadapter/ImageBindingAdapter.kt
@@ -1,9 +1,12 @@
 package com.android.presentation.ui.common.bindingadapter
 
+import android.os.Build.VERSION.SDK_INT
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import coil.ImageLoader
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.load
-import coil.transform.CircleCropTransformation
 
 @BindingAdapter("imageUrl")
 fun loadImage(view: ImageView, imageUrl: String?) {
@@ -14,9 +17,15 @@ fun loadImage(view: ImageView, imageUrl: String?) {
 
 @BindingAdapter("circleImageUrl")
 fun loadCircleImage(view: ImageView, imageUrl: String?) {
-    if (imageUrl.isNullOrEmpty().not()) {
-        view.load(imageUrl) {
-            transformations(CircleCropTransformation())
+    val imageLoader = ImageLoader.Builder(view.context).components {
+        if (SDK_INT >= 28) {
+            add(ImageDecoderDecoder.Factory())
+        } else {
+            add(GifDecoder.Factory())
         }
+    }.build()
+
+    imageUrl?.let {
+        view.load(it, imageLoader)
     }
 }
