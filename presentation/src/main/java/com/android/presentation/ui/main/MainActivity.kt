3783427006a848d6716e50cf4230114c5f89c123
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-07 16:07:10 +0900

    Issues #11 style: toolbar 적용
    - NoActionBar 적용
    - SupportActionBar 적용

--- presentation/src/main/java/com/android/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/android/presentation/ui/main/MainActivity.kt
@@ -3,6 +3,10 @@ package com.android.presentation.ui.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.android.presentation.R
 import com.android.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,5 +20,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(binding.root)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(binding.toolbar)
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()
+        setupNavController(navController)
+    }
+
+    private fun setupNavController(navController: NavController?) {
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment))
+        navController?.let {
+            binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        }
     }
 }
