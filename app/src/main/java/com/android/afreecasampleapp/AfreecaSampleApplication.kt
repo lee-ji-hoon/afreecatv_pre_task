d01d653707a303a55b310b2cbc306aa845219cb0
lee-ji-hoon <dlwlgns1240@naver.com>
2023-01-08 11:40:10 +0900

    Issues #18 docs: 오타 정리 및 popup 메뉴 추가

--- app/src/main/java/com/android/afreecasampleapp/AfreecaSampleApplication.kt
+++ app/src/main/java/com/android/afreecasampleapp/AfreecaSampleApplication.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class BoostSearchApplication : Application() {
+class AfreecaSampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
